7f35823598593a1b3a0c665c97c278bb42bd0336
olivergav <oliwier.gawlik0@gmail.com>
2022-05-02 22:06:47 +0200

    Added HomePage button in deatilView

--- src/components/BreedListAtEnter.js
+++ src/components/BreedListAtEnter.js
@@ -5,10 +5,17 @@ import {Link} from "react-router-dom";
 function BreedListAtEnter({api_key}) {
     const [breeds, setBreeds] = useState([])
 
+    // const [breedsToSearch, setBreedsToSearch] = useState([])
+
+    const [mySearch, setMySearch] = useState('')
+
     useEffect(() => {
-        getBreeds(api_key).then((data) => setBreeds(data))
-        console.log(breeds)
+        getBreeds(api_key).then((data) => setBreeds(data)).then()
     }, []);
+
+    // useEffect(() => {
+    //     setBreedsToSearch(breeds)
+    // }, [])
 
     async function getBreeds(api_key) {
         const response = await fetch(`https://api.TheDogAPI.com/v1/breeds?api_key=${api_key}`)
@@ -20,15 +27,24 @@ function BreedListAtEnter({api_key}) {
         return await response.json();
     }
 
+    function handleSubmit(event){
+        event.preventDefault()
+        // console.log(breeds)
+        // console.log(breedsToSearch)
+        // console.log(mySearch)
+        setBreeds(breeds.filter((dog) => dog.name.toLowerCase().includes(mySearch.toLowerCase())))
+    }
 
     return (
         <>
             <div className="App">
-                {/*<form onSubmit={handleSubmit}>*/}
-                {/*    <label htmlFor="search">Type breed for more info: </label>*/}
-                {/*    <input type="text" id="search" value={value} onChange={handleSearch}/>*/}
-                {/*    <button>Submit</button>*/}
-                {/*</form>*/}
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor="search">Search by breed: </label>
+                    <input type="text" id="search" value={mySearch} onChange={((event) => {
+                        setMySearch(event.target.value)
+                    })}/>
+                    <button>Submit</button>
+                </form>
 
                 <ul>
                     <h1>All breeds:</h1>
---
+++ trudnosci_pytania.md
@@ -0,0 +1,2 @@
+jak zrobić, żeby móc wyszukać psa, a następnie wysłać pustego stringa, żeby pokazały nam się jednak wszystkie psy?
+po wysłaniu formularza nadpisuję moje 'breeds' i nie jestem w stanie go potem podmienić inaczej niż ponownym requestem
